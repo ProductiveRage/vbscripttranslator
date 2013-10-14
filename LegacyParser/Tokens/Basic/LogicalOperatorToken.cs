fbf8b3ab510acfb2a10ead93474c97b6f81f55fe
dan <742348+ProductiveRage@users.noreply.github.com>
2013-10-14 22:23:28 +0100

    Forgot to include class this when added the extended AtomToken information

---
+++ LegacyParser/Tokens/Basic/LogicalOperatorToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace VBScriptTranslator.LegacyParser.Tokens.Basic
+{
+    [Serializable]
+    public class LogicalOperatorToken : OperatorToken
+    {
+        /// <summary>
+        /// This inherits from AtomToken since a lot of processing would consider them the
+        /// same token type while parsing the original content.
+        /// </summary>
+        public LogicalOperatorToken(string content) : base(content)
+        {
+            // Do all this validation (again) here in case this constructor wasn't called
+            // by the AtomToken.GetNewToken method
+            if (content == null)
+                throw new ArgumentNullException("content");
+            if (content == "")
+                throw new ArgumentException("Blank content specified for OperatorToken - invalid");
+            if (!AtomToken.isLogicalOperator(content))
+                throw new ArgumentException("Invalid content specified - not a Logical Operator");
+            this.content = content;
+        }
+    }
+}
