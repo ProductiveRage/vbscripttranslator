33c19d3217150925a05c07d247cc0989968198be
dan <742348+ProductiveRage@users.noreply.github.com>
2013-10-23 15:10:36 +0100

    Added some more unit tests for VBScriptEsqueValueRetriever, covering named and default member access for native types and ADO recordset, plus an array access test

--- UnitTests/CSharpSupport/Implementations/VBScriptEsqueValueRetrieverTests.cs
+++ UnitTests/CSharpSupport/Implementations/VBScriptEsqueValueRetrieverTests.cs
@@ -1,8 +1,7 @@
-﻿using CSharpSupport.Implementations;
+﻿using CSharpSupport;
+using CSharpSupport.Implementations;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Xunit;
 
 namespace VBScriptTranslator.UnitTests.CSharpSupport.Implementations
@@ -159,11 +158,185 @@ namespace VBScriptTranslator.UnitTests.CSharpSupport.Implementations
         }
 
         [Fact]
-        public void IFIgnoresWhiteSpaceWhenParseingStrings()
+        public void IFIgnoresWhiteSpaceWhenParsingStrings()
         {
             Assert.True(
                 (new VBScriptEsqueValueRetriever(name => name)).IF("   1    ")
             );
+        }
+
+        [Fact]
+        public void NativeClassSupportsMethodCallWithArguments()
+        {
+            Assert.Equal(
+                new PseudoField { value = "value:F1" },
+                (new VBScriptEsqueValueRetriever(name => name)).CALL(
+                    new PseudoRecordset(),
+                    new[] { "fields" },
+                    "F1"
+                ),
+                new PseudoFieldObjectComparer()
+            );
+        }
+
+        [Fact]
+        public void NativeClassSupportsDefaultMethodCallWithArguments()
+        {
+            Assert.Equal(
+                new PseudoField { value = "value:F1" },
+                (new VBScriptEsqueValueRetriever(name => name)).CALL(
+                    new PseudoRecordset(),
+                    new string[0],
+                    "F1"
+                ),
+                new PseudoFieldObjectComparer()
+            );
+        }
+
+        [Fact]
+        public void ADORecordsetSupportsNamedFieldAccess()
+        {
+            var recordset = new ADODB.Recordset();
+            recordset.Fields.Append("name", ADODB.DataTypeEnum.adVarChar, 20, ADODB.FieldAttributeEnum.adFldUpdatable);
+            recordset.Open(CursorType: ADODB.CursorTypeEnum.adOpenUnspecified, LockType: ADODB.LockTypeEnum.adLockUnspecified, Options: 0);
+            recordset.AddNew();
+            recordset.Fields["name"].Value = "TestName";
+            recordset.Update();
+
+            Assert.Equal(
+                recordset.Fields["name"],
+                (new VBScriptEsqueValueRetriever(name => name)).CALL(
+                    recordset,
+                    new[] { "fields" },
+                    "name"
+                ),
+                new ADOFieldObjectComparer()
+            );
+        }
+
+        [Fact]
+        public void ADORecordsetSupportsDefaultFieldAccess()
+        {
+            var recordset = new ADODB.Recordset();
+            recordset.Fields.Append("name", ADODB.DataTypeEnum.adVarChar, 20, ADODB.FieldAttributeEnum.adFldUpdatable);
+            recordset.Open(CursorType: ADODB.CursorTypeEnum.adOpenUnspecified, LockType: ADODB.LockTypeEnum.adLockUnspecified, Options: 0);
+            recordset.AddNew();
+            recordset.Fields["name"].Value = "TestName";
+            recordset.Update();
+
+            Assert.Equal(
+                recordset.Fields["name"],
+                (new VBScriptEsqueValueRetriever(name => name)).CALL(
+                    recordset,
+                    new string[0],
+                    "name"
+                ),
+                new ADOFieldObjectComparer()
+            );
+        }
+
+        /// <summary>
+        /// This describes an extremely common VBScript pattern - rstResults("name") needs to return a value by using the default Fields access
+        /// (passing through "name" to it) and then default access of the Field's Value property (which requires a VAL call, which must be
+        /// included in translated code if a value type is expected - any time other than when a SET statement is present as part of a
+        /// variable assignment)
+        /// </summary>
+        [Fact]
+        public void ADORecordsetSupportsDefaultFieldValueAccess()
+        {
+            var recordset = new ADODB.Recordset();
+            recordset.Fields.Append("name", ADODB.DataTypeEnum.adVarChar, 20, ADODB.FieldAttributeEnum.adFldUpdatable);
+            recordset.Open(CursorType: ADODB.CursorTypeEnum.adOpenUnspecified, LockType: ADODB.LockTypeEnum.adLockUnspecified, Options: 0);
+            recordset.AddNew();
+            recordset.Fields["name"].Value = "TestName";
+            recordset.Update();
+
+            var valueRetriever = new VBScriptEsqueValueRetriever(name => name);
+            Assert.Equal(
+                "TestName",
+                valueRetriever.VAL(
+                    valueRetriever.CALL(
+                        recordset,
+                        new string[0],
+                        "name"
+                    )
+                )
+            );
+        }
+
+        [Fact]
+        public void OneDimensionalArrayAccessIsSupported()
+        {
+            var data = new object[] { "One" };
+            Assert.Equal(
+                "One",
+                (new VBScriptEsqueValueRetriever(name => name)).CALL(
+                    data,
+                    new string[0],
+                    0
+                )
+            );
+        }
+
+        private class ADOFieldObjectComparer : IEqualityComparer<object>
+        {
+            public new bool Equals(object x, object y)
+            {
+                if (x == null)
+                    throw new ArgumentNullException("x");
+                if (y == null)
+                    throw new ArgumentNullException("y");
+                var fieldX = x as ADODB.Field;
+                if (fieldX == null)
+                    throw new ArgumentException("x is not an ADODB.Field");
+                var fieldY = y as ADODB.Field;
+                if (fieldY == null)
+                    throw new ArgumentException("y is not an ADODB.Field");
+                return fieldX.Value == fieldY.Value;
+            }
+
+            public int GetHashCode(object obj)
+            {
+                return 0;
+            }
+        }
+
+        private class PseudoFieldObjectComparer : IEqualityComparer<object>
+        {
+            public new bool Equals(object x, object y)
+            {
+                if (x == null)
+                    throw new ArgumentNullException("x");
+                if (y == null)
+                    throw new ArgumentNullException("y");
+                var fieldX = x as PseudoField;
+                if (fieldX == null)
+                    throw new ArgumentException("x is not a PseudoField");
+                var fieldY = y as PseudoField;
+                if (fieldY == null)
+                    throw new ArgumentException("y is not a PseudoField");
+                return (fieldX.value as string) == (fieldY.value as string);
+            }
+
+            public int GetHashCode(object obj)
+            {
+                return 0;
+            }
+        }
+
+        private class PseudoRecordset
+        {
+            [IsDefault]
+            public object fields(string fieldName)
+            {
+                return new PseudoField { value = "value:" + fieldName };
+            }
+        }
+
+        private class PseudoField
+        {
+            [IsDefault]
+            public object value { get; set; }
         }
     }
 }
