f77dc458ee478801d789619c561dc144429e2d13
Dan Roberts <742348+ProductiveRage@users.noreply.github.com>
2013-10-29 20:56:42 +0000

    Added a couple of StatementTranslator unit tests, need a lot more yet!

---
+++ UnitTests/CSharpWriter/CodeTranslation/StatementTranslation/StatementTranslatorTests.cs
@@ -0,0 +1,98 @@
+﻿using CSharpWriter.CodeTranslation;
+using CSharpWriter.CodeTranslation.StatementTranslation;
+using CSharpWriter.Lists;
+using VBScriptTranslator.LegacyParser.Tokens.Basic;
+using VBScriptTranslator.StageTwoParser.ExpressionParsing;
+using VBScriptTranslator.UnitTests.Shared.Comparers;
+using Xunit;
+
+namespace VBScriptTranslator.UnitTests.CSharpWriter.CodeTranslation.StatementTranslation
+{
+	public class StatementTranslatorTests
+	{
+		// TODO: "o" where "o" has a default parameter-less property => try to access that property, doesn't matter if returns value-type or reference
+		// TODO: "o" where "o" has a default parameter-less function => try to access that property, doesn't matter if returns value-type or reference
+
+		// TODO: "WScript.Echo o" where "o" has a default parameter-less property => displays the property value (if value-type, "Type mismatch" if reference)
+		// TODO: "WScript.Echo o" where "o" has a default parameter-less function => "Type mismatch"
+		// TODO: "WScript.Echo o()" where "o" has a default parameter-less function => displays the function return value (if value-type, "Type mismatch" if reference)
+
+		[Fact]
+		public void IsolatedNonFunctionOrPropertyReferenceHasValueTypeAccessLogic()
+		{
+			// "o" (where there is no function or property in scope called "o")
+			var expression = new Expression(new[]
+			{
+				new CallExpressionSegment(
+					new[] { new NameToken("o") },
+					new Expression[0],
+					CallExpressionSegment.ArgumentBracketPresenceOptions.Absent
+				)
+			});
+			var expected = new TranslatedStatementContentDetails(
+				"_.VAL(o)",
+				new NonNullImmutableList<NameToken>(new[] { new NameToken("o") })
+			);
+			var scopeAccessInformation = ScopeAccessInformation.Empty;
+			Assert.Equal(
+				expected,
+				GetDefaultStatementTranslator().Translate(expression, scopeAccessInformation, ExpressionReturnTypeOptions.None),
+				new TranslatedStatementContentDetailsComparer()
+			);
+		}
+
+		[Fact]
+		public void IsolatedFunctionCallAccordingToScopeDoesNotHaveValueTypeAccessLogic()
+		{
+			// "o" (where there is a function in scope called "o")
+			var expression = new Expression(new[]
+			{
+				new CallExpressionSegment(
+					new[] { new NameToken("o") },
+					new Expression[0],
+					CallExpressionSegment.ArgumentBracketPresenceOptions.Absent
+				)
+			});
+			var scopeAccessInformation = new ScopeAccessInformation(
+				null,
+				null,
+				null,
+				new NonNullImmutableList<NameToken>(),
+				new NonNullImmutableList<NameToken>(new[] { new NameToken("o") }), // Functions
+				new NonNullImmutableList<NameToken>(),
+				new NonNullImmutableList<NameToken>()
+			);
+			var expected = new TranslatedStatementContentDetails(
+				"o()",
+				new NonNullImmutableList<NameToken>(new[] { new NameToken("o") })
+			);
+			Assert.Equal(
+				expected,
+				GetDefaultStatementTranslator().Translate(expression, scopeAccessInformation, ExpressionReturnTypeOptions.None),
+				new TranslatedStatementContentDetailsComparer()
+			);
+		}
+
+		private static StatementTranslator GetDefaultStatementTranslator()
+		{
+			return new StatementTranslator(
+				DefaultSupportClassName,
+				DefaultNameRewriter,
+				GetDefaultTempValueNameGenerator()
+			);
+		}
+
+		private static CSharpName DefaultSupportClassName = new CSharpName("_");
+		private static VBScriptNameRewriter DefaultNameRewriter = nameToken => new CSharpName(nameToken.Content.ToLower());
+		private static TempValueNameGenerator GetDefaultTempValueNameGenerator()
+		{
+			var index = 0;
+			return optionalPrefix =>
+			{
+				var name = optionalPrefix.Name + "_tempVal" + index;
+				index++;
+				return new CSharpName(name);
+			};
+		}
+	}
+}
---
+++ UnitTests/Shared/Comparers/TranslatedStatementContentDetailsComparer.cs
@@ -0,0 +1,31 @@
+﻿using CSharpWriter.CodeTranslation;
+using System;
+using System.Collections.Generic;
+
+namespace VBScriptTranslator.UnitTests.Shared.Comparers
+{
+	public class TranslatedStatementContentDetailsComparer : IEqualityComparer<TranslatedStatementContentDetails>
+	{
+		public bool Equals(TranslatedStatementContentDetails x, TranslatedStatementContentDetails y)
+		{
+			if (x == null)
+				throw new ArgumentNullException("x");
+			if (y == null)
+				throw new ArgumentNullException("y");
+
+			if (x.TranslatedContent != y.TranslatedContent)
+				return false;
+
+			var tokenSetComparer = new TokenSetComparer();
+			return tokenSetComparer.Equals(x.VariablesAccesed, y.VariablesAccesed);
+		}
+
+		public int GetHashCode(TranslatedStatementContentDetails obj)
+		{
+			if (obj == null)
+				throw new ArgumentNullException("obj");
+
+			return 0;
+		}
+	}
+}
