1020e2437de8112a1d34f5c195189ff93f0766d9
dan <742348+ProductiveRage@users.noreply.github.com>
2014-04-22 20:35:52 +0100

    Additional work done to the Statement class' GetBracketStandardisedTokens method to ensure that when brackets are normalised for Statements, any brackets that were present that would force an argument to be passed ByVal (where otherwise it would be elligible to be passed ByRef) are maintained

--- LegacyParser/CodeBlocks/Basic/Statement.cs
+++ LegacyParser/CodeBlocks/Basic/Statement.cs
@@ -37,7 +37,7 @@ namespace VBScriptTranslator.LegacyParser.CodeBlocks.Basic
             if ((firstTokenAsAtom != null) && firstTokenAsAtom.Content.Equals("Call", StringComparison.InvariantCultureIgnoreCase))
                 throw new ArgumentException("The first token may not be the Call keyword, that must be specified through the CallPrefixOption value where present");
 
-            BracketStandardisedTokens = GetBracketStandardisedTokens(Tokens);
+            BracketStandardisedTokens = GetBracketStandardisedTokens(Tokens, callPrefix);
             CallPrefix = callPrefix;
         }
 
@@ -117,19 +117,133 @@ namespace VBScriptTranslator.LegacyParser.CodeBlocks.Basic
         //   "a(0).Test 1" bad
         //   "a(0).Test(1)" good
         // Note: Allow "Test" or "a.Test", if there are no arguments then it's less important
+        // 2014-03-24 DWR: This has been tightened up since "Test a" and "Test(a)" are not the
+        // same to VBScript, the latter actually associates the brackets with the "a" and not
+        // the "Test" call, brackets around an argument to function mean that the argument
+        // should be passed ByVal, not ByRef - if the argument would otherwise be ByRef.
+        // This is why "Test(1, 2)" is not valid (the error "Cannot use parentheses when calling
+        // a Sub" will be raised) since the parentheses are associated with the function call,
+        // which is not acceptable when not considering the return value - eg. "a = Test(1, 2)"
+        // IS acceptable since the return value IS being considered. As such, the following
+        // transformations should be applied:
+        //   "Test a1" => "Test(a1)" (argument not forced to be ByVal)
+        //   "Test(a1)" => "Test((a1))" (argument forced to be ByVal)
+        //   "r = Test a1" is invalid ("Expected end of statement")
+        //   "r = Test(a1)" requires no transformation (ByVal not enforced)
+        //   "r = Test((a1))" requires no transformation (ByVal IS enforced)
+        //   "Test(a1, a2)" is invalid ("Cannot use parentheses when calling a Sub")
+        //   "r = Test(a1, a2)" requires no transformation (ByVal not enforced)
+        //   "r = Test((a1), a2)" requires no transformation (ByVal enforced for a1 but not a2)
+        //   "CALL Test(a1, a2)" requires no transformation (ByVal not enforced)
+        //   "CALL Test((a1), a2)" requires no transformation (ByVal enforced for a1 but not a2)
+        // See http://blogs.msdn.com/b/ericlippert/archive/2003/09/15/52996.aspx
+        // (In all of the above examples, the value-setting statements such as "r = Test(a1)"
+        // would not be represented by this class, the ValueSettingStatment class would be used,
+        // but the examples are included to illustrate variations on the cases that must be
+        // dealt with),
         // =======================================================================================
-        private static IEnumerable<IToken> GetBracketStandardisedTokens(IEnumerable<IToken> tokens)
+        private static IEnumerable<IToken> GetBracketStandardisedTokens(IEnumerable<IToken> tokens, CallPrefixOptions callPrefix)
         {
             if (tokens == null)
                 throw new ArgumentNullException("tokens");
+            if (!Enum.IsDefined(typeof(CallPrefixOptions), callPrefix))
+                throw new ArgumentOutOfRangeException("callPrefix");
 
             var tokenArray = tokens.ToArray();
             if (tokenArray.Length == 0)
                 throw new ArgumentException("Empty tokens set specified - invalid for a Statement");
 
+            // TODO: Add a method that will detect all forms of invalid content first and require that this method be called before the Statement is
+            // processed further (dealing with all run time or compile time errors). Such a method would enable the work to be performed under the
+            // assumption that the content was valid VBScript, which would make things easier. (Should this method be in a translator and this data
+            // be returned from a method GetBracketStandardisedTokensIfContentValid?)
+
             // No need to try to re-arrange things if this is a new-instance expression, no brackets are required
             if ((tokenArray[0] is KeyWordToken) && tokenArray[0].Content.Equals("new", StringComparison.InvariantCultureIgnoreCase))
                 return tokenArray;
+
+            // 2014-04-21 DWR: Require some preliminary work to deal with "Test -1" such that it will be parsed as "Test(-1)" rather than an attempted
+            // subtraction of 1 from Test. If Test is not a function with a single argument then there will be a runtime error raised. The case where
+            // brackets are absent is only possible where CALL is not used (since that would make brackets usage compulsory).
+            if (callPrefix == CallPrefixOptions.Absent)
+            {
+                var valueTokenTypes = new[] {
+                    typeof(BuiltInFunctionToken),
+                    typeof(BuiltInValueToken),
+                    typeof(KeyWordToken),
+                    typeof(NameToken),
+                    typeof(NumericValueToken),
+                    typeof(StringToken)
+                };
+                var firstItemTokens = new List<IToken>
+                {
+                    tokenArray[0]
+                };
+                for (var tokenIndex = 1; tokenIndex < tokenArray.Length; tokenIndex++)
+                {
+                    var token = tokenArray[tokenIndex];
+                    if (token is MemberAccessorOrDecimalPointToken)
+                    {
+                        // If we assume valid content (which should be reasonable for the purposes of trying to pick up on this special case), then a
+                        // MemberAccessorOrDecimalPointToken must indicate a continuation of the current term
+                        firstItemTokens.Add(token);
+                        continue;
+                    }
+                    if (valueTokenTypes.Contains(token.GetType()))
+                    {
+                        if (valueTokenTypes.Contains(firstItemTokens.Last().GetType()))
+                        {
+                            // If there are two adjacent value type tokens then they must be part of two terms - eg. "Test a" (if they were part of
+                            // the same term then there would be a "joining" token such as a MemberAccessorOrDecimalPointToken - eg. "Test.a"). If
+                            // we have reached this point without encountering a minus sign then the special case does not apply to this content.
+                            break;
+                        }
+                    }
+
+                    // If an open bracket is encountered then it must be bracketing an argument meaning that it will be treated as an "enforced ByVal"
+                    // and so ADDITIONAL brackets are going to be required so that this information is not lost in the "standardised bracket" data. If
+                    // a minus sign is encountered then it must be part of the first argument in a method call (until this point we wouldn't have known
+                    // if it was an subtraction operation between two terms or if it was this case, where the minus sign is a negation of the second
+                    // term, which is passed as an argument to the first term).
+                    var isMinusSign = (token is OperatorToken) && (token.Content == "-");
+                    var isOpenBrace = token is OpenBrace;
+                    if (isMinusSign || isOpenBrace)
+                    {
+                        var remainingTokens = tokenArray.Skip(tokenIndex).ToArray();
+                        if (isMinusSign && (remainingTokens.Length > 1))
+                        {
+                            // If this is a minus operator that starts the second term, but no brackets have been encountered, then insert brackets
+                            // around the content that starts here (close the bracket at the end of the token set). VBScript gives special meaning to
+                            // brackets in some cases - eg. "Test (a)" will mean that the argument is passed ByVal even when it would otherwise by ByRef,
+                            // but the case that we're testing for here is where the first argument is made negative, which would mean it could never be
+                            // passed ByRef anyway and so the change we do here can not have any functional impact.
+                            var numericValue = remainingTokens[1] as NumericValueToken;
+                            if (numericValue != null)
+                            {
+                                // If the first token in the to-be-bracketed content following the minus sign is a number then we can combine the
+                                // minus operator and the number into a single token (this is similar to some of the work performed by the
+                                // NumberRebuilder except that it doesn't analyse the code at this level and so could not pick up on this
+                                // particular combination)
+                                remainingTokens = new[] { new NumericValueToken(-numericValue.Value, numericValue.LineIndex) }
+                                    .Concat(remainingTokens.Skip(2))
+                                    .ToArray();
+                            }
+                        }
+                        var lineIndexForInsertedOpenBrace = firstItemTokens.Last().LineIndex;
+                        var lineIndexForInsertedCloseBrace = remainingTokens.Any() ? remainingTokens.Last().LineIndex : lineIndexForInsertedOpenBrace;
+                        tokenArray = firstItemTokens
+                            .Concat(new[] { new OpenBrace(lineIndexForInsertedOpenBrace) })
+                            .Concat(remainingTokens)
+                            .Concat(new[] { new CloseBrace(lineIndexForInsertedCloseBrace) })
+                            .ToArray();
+                        break;
+                    }
+
+                    // Any other token types encountered here (an operator, for example) mean that the special cases were not encountered and we can
+                    // drop out of this loop
+                    break;
+                }
+            }
 
             var bracketCount = 0;
             IToken lastUnbracketedToken = null;
--- UnitTests/LegacyParser/StatementBracketStandardisingTest.cs
+++ UnitTests/LegacyParser/StatementBracketStandardisingTest.cs
@@ -26,24 +26,6 @@ namespace VBScriptTranslator.UnitTests.LegacyParser
             }
 
             [Fact]
-            public void DirectMethodWithSingleArgumentWithBrackets()
-            {
-                var tokens = new IToken[]
-                {
-                    new NameToken("Test", 0),
-                    new OpenBrace(0),
-                    new NumericValueToken(1, 0),
-                    new CloseBrace(0)
-                };
-                var statement = new Statement(tokens, Statement.CallPrefixOptions.Absent);
-                Assert.Equal(
-                    tokens,
-                    statement.BracketStandardisedTokens,
-                    new TokenSetComparer()
-                );
-            }
-
-            [Fact]
             public void DirectMethodWithSingleArgumentWithBracketsAndCallKeyword()
             {
                 var tokens = new IToken[]
@@ -102,10 +84,212 @@ namespace VBScriptTranslator.UnitTests.LegacyParser
                     new TokenSetComparer()
                 );
             }
+
+            [Fact]
+            public void FunctionCallWithCallKeywordAndMandatoryArgumentBrackets()
+            {
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new OpenBrace(0),
+                    new NameToken("a", 0),
+                    new CloseBrace(0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Present);
+                Assert.Equal(
+                    tokens,
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
+
+            [Fact]
+            public void FunctionCallWithCallKeywordAndMandatoryAndAdditionalArgumentBrackets()
+            {
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new OpenBrace(0),
+                    new OpenBrace(0),
+                    new NameToken("a", 0),
+                    new CloseBrace(0),
+                    new CloseBrace(0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Present);
+                Assert.Equal(
+                    tokens,
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
+
+            [Fact]
+            public void FunctionCallWithCallKeywordAndMandatoryArgumentBracketsAroundNegatedVariable()
+            {
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new OpenBrace(0),
+                    new OperatorToken("-", 0),
+                    new NameToken("a", 0),
+                    new CloseBrace(0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Present);
+                Assert.Equal(
+                    tokens,
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
+
+            [Fact]
+            public void FunctionCallWithCallKeywordAndMandatoryArgumentBracketsAroundNegatedConstant()
+            {
+                // For statements such as "CALL Test(-1)" the "-" and "1" tokens should have been combined before passing them to the
+                // Statement constructor. This is unlike "Test -1" since the logic for ensuring that this is considered "Test(-1)"
+                // rather than the value "Test" minus one is in the Statement class, work before that point is unaware of it.
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new OpenBrace(0),
+                    new NumericValueToken(-1, 0),
+                    new CloseBrace(0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Present);
+                Assert.Equal(
+                    tokens,
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
         }
 
         public class ChangingTests
         {
+            /// <summary>
+            /// The statement "Test(a)" treats the brackets as passing "a" ByVal to Test, even if the argument for Test is specified as ByRef,
+            /// the brackets are not being used to associate the argument(s) with the function call. So additional brackets are required so
+            /// that ByVal-enforcing brackets are still present within the standardised format (where brackets MUST be used to associate
+            /// arguments with the target function - this is the whole point of the bracket standardising process).
+            /// </summary>
+            [Fact]
+            public void DirectMethodWithSingleArgumentWithBrackets()
+            {
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new OpenBrace(0),
+                    new NameToken("a", 0),
+                    new CloseBrace(0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Absent);
+                Assert.Equal(
+                    new IToken[]
+                    {
+                        new NameToken("Test", 0),
+                        new OpenBrace(0),
+                        new OpenBrace(0),
+                        new NameToken("a", 0),
+                        new CloseBrace(0),
+                        new CloseBrace(0)
+                    },
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
+
+            /// <summary>
+            /// The same logic that applies to DirectMethodWithSingleArgumentWithBrackets applies if an argument already appears to be
+            /// "double wrapped", there is no logic to remove extra brackets that are not required
+            /// </summary>
+            [Fact]
+            public void DirectMethodWithSingleArgumentWithDoubleBrackets()
+            {
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new OpenBrace(0),
+                    new OpenBrace(0),
+                    new NameToken("a", 0),
+                    new CloseBrace(0),
+                    new CloseBrace(0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Absent);
+                Assert.Equal(
+                    new IToken[]
+                    {
+                        new NameToken("Test", 0),
+                        new OpenBrace(0),
+                        new OpenBrace(0),
+                        new OpenBrace(0),
+                        new NameToken("a", 0),
+                        new CloseBrace(0),
+                        new CloseBrace(0),
+                        new CloseBrace(0)
+                    },
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
+
+            [Fact]
+            public void DirectMethodWithSingleNegativeArgumentWithoutBrackets()
+            {
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new OperatorToken("-", 0),
+                    new NameToken("a", 0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Absent);
+                Assert.Equal(
+                    new IToken[]
+                    {
+                        new NameToken("Test", 0),
+                        new OpenBrace(0),
+                        new OperatorToken("-", 0),
+                        new NameToken("a", 0),
+                        new CloseBrace(0)
+                    },
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
+
+            /// <summary>
+            /// Since "Test -1" is confirmed to mean "Test(-1)" by the Statement bracket-standardising process (rather than possibly
+            /// meaning "Test(-1)" or possibly meaning the value "Test" minus one - until the Statement class comes into play, nothing
+            /// in the processing chain makes that decision) the "-" and "1" tokens can be combined since it is clear that they are
+            /// part of the same value and the "-" token is not an operator acting on "Test" and "1".
+            /// </summary>
+            [Fact]
+            public void DirectMethodWithSingleNegativeNumericArgumentWithoutBrackets()
+            {
+                // From a previous comment on test "TestMinusOneIsMethodCallNotSubtractionOperation" (which was written but not
+                // committed in - and it was a duplicate of this test):
+                //   The statement "Test -1" must be interpreted at "Test(-1)", not a subtraction of 1 from Test. If "Test" is an
+                //   argument-less function then it will be executed to perform the work of the statement. If it is not (if "Test"
+                //   is a variable rather than a function or property) then a runtime error will be raised ("Type mismatch").
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new OperatorToken("-", 0),
+                    new NumericValueToken(1, 0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Absent);
+                Assert.Equal(
+                    new IToken[]
+                    {
+                        new NameToken("Test", 0),
+                        new OpenBrace(0),
+                        new NumericValueToken(-1, 0),
+                        new CloseBrace(0)
+                    },
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
+
             [Fact]
             public void DirectMethodWithSingleArgumentWithoutBrackets()
             {
@@ -157,6 +341,99 @@ namespace VBScriptTranslator.UnitTests.LegacyParser
                     new TokenSetComparer()
                 );
             }
+
+            [Fact]
+            public void DirectMethodWithTwoArgumentsWithoutBrackets()
+            {
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new NumericValueToken(1, 0),
+                    new ArgumentSeparatorToken(",", 0),
+                    new NumericValueToken(2, 0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Absent);
+                Assert.Equal(
+                    new IToken[]
+                    {
+                        new NameToken("Test", 0),
+                        new OpenBrace(0),
+                        new NumericValueToken(1, 0),
+                        new ArgumentSeparatorToken(",", 0),
+                        new NumericValueToken(2, 0),
+                        new CloseBrace(0)
+                    },
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
+
+            /// <summary>
+            /// As with DirectMethodWithSingleNegativeNumericArgumentWithoutBrackets, a negative sign operator can be combined with a
+            /// number to make a negative value if it is the first argument (until the bracket-standardising process, it could have
+            /// meant a subtraction operation as far as we knew)
+            /// </summary>
+            [Fact]
+            public void DirectMethodWithTwoArgumentsWithoutBracketsWhereFirstTermIsNegative()
+            {
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new OperatorToken("-", 0),
+                    new NumericValueToken(1, 0),
+                    new ArgumentSeparatorToken(",", 0),
+                    new NumericValueToken(2, 0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Absent);
+                Assert.Equal(
+                    new IToken[]
+                    {
+                        new NameToken("Test", 0),
+                        new OpenBrace(0),
+                        new NumericValueToken(-1, 0),
+                        new ArgumentSeparatorToken(",", 0),
+                        new NumericValueToken(2, 0),
+                        new CloseBrace(0)
+                    },
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
+
+            [Fact]
+            public void NestedMethodCallWithArgumentsShouldNotGetDoubleBrackets()
+            {
+                // "Test Test2(a)" should be transformed into "Test(Test2(a))", the brackets around the "a" do not imply that it
+                // should be passed ByVal, they are required as the call to Test2 is one where the value is considered (brackets
+                // are only only in statements where the return value is not considered - the way in which the value "Test2(a)"
+                // is passed to "Test", for example.
+                var tokens = new IToken[]
+                {
+                    new NameToken("Test", 0),
+                    new NameToken("Test2", 0),
+                    new OpenBrace(0),
+                    new NameToken("a", 0),
+                    new CloseBrace(0)
+                };
+                var statement = new Statement(tokens, Statement.CallPrefixOptions.Absent);
+                Assert.Equal(
+                    new IToken[]
+                    {
+                        new NameToken("Test", 0),
+                        new OpenBrace(0),
+                        new NameToken("Test2", 0),
+                        new OpenBrace(0),
+                        new NameToken("a", 0),
+                        new CloseBrace(0),
+                        new CloseBrace(0)
+                    },
+                    statement.BracketStandardisedTokens,
+                    new TokenSetComparer()
+                );
+            }
         }
+        
+        // TODO
+        //"Test Test2(1)" // Works - CHANGING
     }
 }
