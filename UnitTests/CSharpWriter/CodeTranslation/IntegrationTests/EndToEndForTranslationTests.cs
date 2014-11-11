634c532129ed11b5e1c8ee0567cc643f29d0f140
dan <742348+ProductiveRage@users.noreply.github.com>
2014-11-11 23:02:22 +0000

    Added a couple more end-to-end FOR loop translation tests (there are still notes present about more to add)

--- UnitTests/CSharpWriter/CodeTranslation/IntegrationTests/EndToEndForTranslationTests.cs
+++ UnitTests/CSharpWriter/CodeTranslation/IntegrationTests/EndToEndForTranslationTests.cs
@@ -14,7 +14,7 @@ namespace VBScriptTranslator.UnitTests.CSharpWriter.CodeTranslation.IntegrationT
             ";
             var expected = new[]
             {
-                "for (_outer.i = 1; _.NUM(_outer.i) < 5; _outer.i = _.NUM(_outer.i) + 1)",
+                "for (_outer.i = 1; _.NUM(_outer.i) <= 5; _outer.i = _.NUM(_outer.i) + 1)",
                 "{",
                 "}"
             };
@@ -23,6 +23,44 @@ namespace VBScriptTranslator.UnitTests.CSharpWriter.CodeTranslation.IntegrationT
                 WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
             );
         }
+
+        [Fact]
+        public void DescendingLoopWithoutExplicitStepIsOptimisedOut()
+        {
+            // If the loop range is in the opposite direction to step then it will never be entered in VBScript and so there's no pointing emitting any C# code (this
+            // can only be done if the loop start, end and step are known at compile time - here the start and end are numeric and the loop is implicitly one)
+            var source = @"
+                Dim i: For i = 5 To 1
+                Next
+            ";
+            Assert.Equal(
+                new string[0],
+                WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
+            );
+        }
+
+        [Fact]
+        public void ZeroStepResultsInInfiniteLoopWhenAscending()
+        {
+            var source = @"
+                Dim i: For i = 1 To 5 Step 0
+                Next
+            ";
+            var expected = new[]
+            {
+                "for (_outer.i = 1; _.NUM(_outer.i) <= 5;)",
+                "{",
+                "}"
+            };
+            Assert.Equal(
+                expected.Select(s => s.Trim()).ToArray(),
+                WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
+            );
+        }
+
+        // TODO public void ZeroStepIsOptimisedOutForDescendingLoop()
+
+
 
         // TODO: Fixed-ascending with step
 
