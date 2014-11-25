9399c78206cf11e5f2d7cf5cff295fc417023dbc
dan <742348+ProductiveRage@users.noreply.github.com>
2014-11-25 21:07:04 +0000

    Forgot to include this in the "Fix to ExpressionGenerator to ensure that NOT operations.." changeset!

---
+++ UnitTests/Shared/Comparers/BuiltInValueExpressionSegmentComparer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using VBScriptTranslator.StageTwoParser.ExpressionParsing;
+
+namespace VBScriptTranslator.UnitTests.Shared.Comparers
+{
+    public class BuiltInValueExpressionSegmentComparer : IEqualityComparer<BuiltInValueExpressionSegment>
+    {
+        public bool Equals(BuiltInValueExpressionSegment x, BuiltInValueExpressionSegment y)
+        {
+            if (x == null)
+                throw new ArgumentNullException("x");
+            if (y == null)
+                throw new ArgumentNullException("y");
+
+            var tokenComparer = new TokenComparer();
+            return tokenComparer.Equals(x.Token, y.Token);
+        }
+
+        public int GetHashCode(BuiltInValueExpressionSegment obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException("obj");
+
+            return 0;
+        }
+    }
+}
