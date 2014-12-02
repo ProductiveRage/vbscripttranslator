36d0fec61589a2e25503a104e1fe12acf73089cc
dan <742348+ProductiveRage@users.noreply.github.com>
2014-12-02 21:34:56 +0000

    Added a unit test for an error-trapping For Each loop

--- UnitTests/CSharpWriter/CodeTranslation/IntegrationTests/EndToEndForEachTranslationTests.cs
+++ UnitTests/CSharpWriter/CodeTranslation/IntegrationTests/EndToEndForEachTranslationTests.cs
@@ -9,7 +9,7 @@ namespace VBScriptTranslator.UnitTests.CSharpWriter.CodeTranslation.IntegrationT
         public void SimpleCaseWithoutErrorHandling()
         {
             var source = @"
-                For Each value IN values
+                For Each value In values
                     WScript.Echo value
                 Next
             ";
@@ -17,8 +17,42 @@ namespace VBScriptTranslator.UnitTests.CSharpWriter.CodeTranslation.IntegrationT
             {
                 "for each (_env.value in _.ENUMERABLE(_env.values)",
                 "{",
-                "_.CALL(_env.wscript, \"echo\", _.ARGS.Ref(_env.value, v1 => { _env.value = v1; }));",
+                "    _.CALL(_env.wscript, \"echo\", _.ARGS.Ref(_env.value, v1 => { _env.value = v1; }));",
                 "}"
+            };
+            Assert.Equal(
+                expected.Select(s => s.Trim()).ToArray(),
+                WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
+            );
+        }
+
+        /// <summary>
+        /// Error-trapping with For Each loops is must less complicated than For loops (where error-trapping is required around the constraints evaluation - if
+        /// they're not compile-time constants - and then distinct error-trapping around the loop, if the constraint evaluation didn't fail)
+        /// </summary>
+        [Fact]
+        public void SimpleCaseWithErrorHandling()
+        {
+            var source = @"
+                On Error Resume Next
+                For Each value In values
+                    WScript.Echo value
+                Next
+            ";
+            var expected = new[]
+            {
+                "var errOn1 = _.GETERRORTRAPPINGTOKEN();",
+                "_.STARTERRORTRAPPING(errOn1);",
+                "_.HANDLEERROR(errOn1, () =>",
+                "{",
+                "    for each (_env.value in _.ENUMERABLE(_env.values)",
+                "    {",
+                "        _.HANDLEERROR(errOn1, () => {",
+                "            _.CALL(_env.wscript, \"echo\", _.ARGS.Ref(_env.value, v2 => { _env.value = v2; }));",
+                "        });",
+                "    }",
+                "});",
+                "_.RELEASEERRORTRAPPINGTOKEN(errOn1);"
             };
             Assert.Equal(
                 expected.Select(s => s.Trim()).ToArray(),
