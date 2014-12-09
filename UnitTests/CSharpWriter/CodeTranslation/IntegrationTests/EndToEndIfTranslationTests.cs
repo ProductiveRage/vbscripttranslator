c11826655c139843cc2bc6efd03b5a4534c503fc
dan <742348+ProductiveRage@users.noreply.github.com>
2014-12-09 21:58:37 +0000

    Replaced TODO comments added yesterday to the IfBlockTranslator unit tests with actual unit tests (illustrates the crazy logic around comparisons where one side is a numeric constant)

--- UnitTests/CSharpWriter/CodeTranslation/IntegrationTests/EndToEndIfTranslationTests.cs
+++ UnitTests/CSharpWriter/CodeTranslation/IntegrationTests/EndToEndIfTranslationTests.cs
@@ -104,10 +104,124 @@ namespace VBScriptTranslator.UnitTests.CSharpWriter.CodeTranslation.IntegrationT
             );
         }
 
-        // TODO: Test 1 against 2 (no cast required, as both already numeric)
-        // TODO: Test "aa" against vbNull (no cast required - builtin values are not treated as numeric constants, so vbNull does not require the string be cast)
-        // TODO: Test False against 1 (cast IS required - builtin value false is not treated as a numeric constant and so must be cast)
-        // TODO: Test "aa" against (1+0) (no cast is required since the RHS is an operation, not a numeric constant)
-        // TODO: Test "aa" against (10 (cast IS required since the redundant brackets are ignored)
+        /// <summary>
+        /// If both sides of a comparison are numeric constants, then there is no casting required since they are both known to be numeric at compile
+        /// time (the test above illustrates what happens if one side is a numeric constant and the other isn't, this to show that that rule only
+        /// holds when one side is a numeric constant and the other isn't, NOT just when one side is a numeric constant - the logic considers
+        /// both sides of the operation)
+        /// </summary>
+        [Fact]
+        public void NumericConstantComparedToAnotherNumericConstantDoesNotRequireAdditionalParsing()
+        {
+            var source = @"
+			    If (1 = 2) Then
+			    End If
+            ";
+            var expected = new[]
+            {
+                "if (_.IF(_.EQ(1, 2)))",
+                "{",
+                "}"
+            };
+            Assert.Equal(
+                expected.Select(s => s.Trim()).ToArray(),
+                WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
+            );
+        }
+
+        /// <summary>
+        /// Even though built-in numeric constants (such as vbNull) are known to be numeric constants at compile time, they do NOT trigger the behaviour
+        /// that is seen when a "real" numeric constant appears in a comparison (if the other side of the operation is a string, this is not forced to
+        /// be parsed into a numeric value)
+        /// </summary>
+        [Fact]
+        public void NumbericBuiltInValuesDoNotCountAsNumericConstantsWhenMakingComparisons()
+        {
+            var source = @"
+			    If (""aa"" = vbNull) Then
+			    End If
+            ";
+            var expected = new[]
+            {
+                "if (_.IF(_.EQ(\"aa\", _.Constants.vbNull)))",
+                "{",
+                "}"
+            };
+            Assert.Equal(
+                expected.Select(s => s.Trim()).ToArray(),
+                WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
+            );
+        }
+
+        /// <summary>
+        /// Although boolean values are often treated as numbers in VBScript, if a comparison to a numeric constant is made then that boolean value
+        /// must be translated into a number, just to be sure
+        /// </summary>
+        [Fact]
+        public void BooleansDoNotGetTreatedAsNumericConstants()
+        {
+            var source = @"
+			    If (False = 1) Then
+			    End If
+            ";
+            var expected = new[]
+            {
+                "if (_.IF(_.EQ(_.NUM(_.Constants.False), 1)))",
+                "{",
+                "}"
+            };
+            Assert.Equal(
+                expected.Select(s => s.Trim()).ToArray(),
+                WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
+            );
+        }
+
+        /// <summary>
+        /// It seems feasible that calculations that are constructed only of numeric constants would be treated as a numeric constant - eg. (1+0)
+        /// could be identified at compile time as effectively being a numeric constant value. The VBScript interpreter does not do this, so if
+        /// a string is compared to (1+0) then the string will not be parsed into a number before the runtime comparison method is called.
+        /// </summary>
+        [Fact]
+        public void CalculationsThatCouldBeEvaluatedAsNumericConstantsAreNotTreatedAsNumericConstants()
+        {
+            var source = @"
+			    If (""aa"" = (1+0)) Then
+			    End If
+            ";
+            var expected = new[]
+            {
+                "if (_.IF(_.EQ(\"aa\", _.ADD(1, 0))))",
+                "{",
+                "}"
+            };
+            Assert.Equal(
+                expected.Select(s => s.Trim()).ToArray(),
+                WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
+            );
+        }
+
+        /// <summary>
+        /// CalculationsThatCouldBeEvaluatedAsNumericConstantsAreNotTreatedAsNumericConstants may suggest that if numeric constants are bracketed
+        /// away then they are no longer treated as numeric constants. This is not the case, though, unnecessary brackets are removed and so any
+        /// string that is compared to that value must be parsed into a numeric value (or an error raised) before the comparison is made.
+        /// </summary>
+        [Fact]
+        public void UnnecessaryBracketsAreUnrolledFromNumericConstants()
+        {
+            var source = @"
+			    If (""aa"" = (1)) Then
+			    End If
+            ";
+            var expected = new[]
+            {
+                "if (_.IF(_.EQ(_.NUM(\"aa\"), 1)))",
+                "{",
+                "}"
+            };
+            Assert.Equal(
+                expected.Select(s => s.Trim()).ToArray(),
+                WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
+            );
+        }
     }
 }
