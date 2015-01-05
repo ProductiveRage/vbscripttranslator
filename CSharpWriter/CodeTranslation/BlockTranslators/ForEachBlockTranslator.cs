350ed60f000a8150f250a0b774f9cd0bb6135603
dan <742348+ProductiveRage@users.noreply.github.com>
2015-01-05 22:57:26 +0000

    Fixes to FOR EACH translation logic to deal more accurately with how VBScript handles errors in the loop when ON ERROR RESUME NEXT comes into play

--- CSharpWriter/CodeTranslation/BlockTranslators/ForEachBlockTranslator.cs
+++ CSharpWriter/CodeTranslation/BlockTranslators/ForEachBlockTranslator.cs
@@ -44,13 +44,54 @@ namespace CSharpWriter.CodeTranslation.BlockTranslators
             if (indentationDepth < 0)
                 throw new ArgumentOutOfRangeException("indentationDepth", "must be zero or greater");
 
-            // The looped-over content must be of type "Reference" since VBScript won't enumerate over strings, for example, whereas C# would be happy to.
+            // Note: The looped-over content must be of type "Reference" since VBScript won't enumerate over strings, for example, whereas C# would be happy to.
             // However, to make the output marginally easier to read, the ENUMERABLE method will deal with this logic and so the ExpressionReturnTypeOptions
             // value passed to the statement translator is "NotSpecified".
+            var loopSourceContent = _statementTranslator.Translate(forEachBlock.LoopSrc, scopeAccessInformation, ExpressionReturnTypeOptions.NotSpecified);
+            var undeclaredVariablesInLoopSourceContent = loopSourceContent.GetUndeclaredVariablesAccessed(scopeAccessInformation, _nameRewriter);
+            foreach (var undeclaredVariable in undeclaredVariablesInLoopSourceContent)
+                _logger.Warning("Undeclared variable: \"" + undeclaredVariable.Content + "\" (line " + (undeclaredVariable.LineIndex + 1) + ")");
+
             var translationResult = TranslationResult.Empty;
+            var enumerationContent = string.Format(
+                "{0}.ENUMERABLE({1})",
+                _supportRefName.Name,
+                loopSourceContent.TranslatedContent
+            );
+            var rewrittenLoopVarName = _nameRewriter.GetMemberAccessTokenName(forEachBlock.LoopVar);
+            var loopVarTargetContainer = scopeAccessInformation.GetNameOfTargetContainerIfAnyRequired(rewrittenLoopVarName, _envRefName, _outerRefName, _nameRewriter);
+            if (loopVarTargetContainer != null)
+                rewrittenLoopVarName = loopVarTargetContainer.Name + "." + rewrittenLoopVarName;
             if (scopeAccessInformation.ErrorRegistrationTokenIfAny != null)
             {
+                // If ON ERROR RESUME NEXT wraps a FOR EACH loop and there is an error in evaluating the enumerator, then the loop will be entered once. The
+                // loop variable will not be altered - eg.
+                //
+                //   On Error Resume Next
+                //   Dim i: For Each i in "12"
+                //     WScript.Echo "We're in the loop! i is " & TypeName(i)
+                //   Next
+                //
+                // VBScript can not enumerate a string, so the loop errors. But the ON ERROR RESUME NEXT causes the loop to be processed - only once. This is
+                // approached by calling _.ENUMERABLE before the loop construct, setting a temporary variable to be the returned enumerable inside a call to
+                // "HANDLEERROR" - meaning that it will be left as null if it fails. Null values are replaced with a single-item array, where the element is
+                // the current value of the loop variable - so its value is not altered when the loop is entered.
+                // - Note: The "error-trapping" wrapper functions ("HANDLEERROR") are used around code that MAY have error-trapping enabled (there are cases
+                //   where we can't know at translation time whether it will have been turned off with ON ERROR GOTO 0 or not - and there are probably some
+                //   cases that could be picked up if the translation process was more intelligent). If the above example had an ON ERROR GOTO 0 between the
+                //   ON ERROR RESUME NEXT and the FOR EACH loop then the error (about trying to enumerate a string) would be raised and so the FOR EACH would
+                //   not be entered, and so the single-element "fallback array" would never come to exist. If errors WERE still being captured, then the
+                //   translated FOR EACH loop WOULD be entered and enumerated through once (without the value of the loop variable being altered, as
+                //   is consistent with VBScript)
+                var enumerationContentVariableName = _tempNameGenerator(new CSharpName("enumerationContent"), scopeAccessInformation);
                 translationResult = translationResult
+                    .Add(new TranslatedStatement(
+                        string.Format(
+                            "IEnumerable {0} = null;",
+                            enumerationContentVariableName.Name
+                        ),
+                        indentationDepth
+                    ))
                     .Add(new TranslatedStatement(
                         string.Format(
                             "{0}.HANDLEERROR({1}, () => {{",
@@ -58,24 +99,24 @@ namespace CSharpWriter.CodeTranslation.BlockTranslators
                             scopeAccessInformation.ErrorRegistrationTokenIfAny.Name
                         ),
                         indentationDepth
-                    ));
-                indentationDepth++;
+                    ))
+                    .Add(new TranslatedStatement(
+                        string.Format(
+                            "{0} = {1}",
+                            enumerationContentVariableName.Name,
+                            enumerationContent
+                        ),
+                        indentationDepth + 1
+                    ))
+                    .Add(new TranslatedStatement("}", indentationDepth));
+                enumerationContent = enumerationContentVariableName.Name + " ?? new object[] { " + rewrittenLoopVarName + " }";
             }
-            var loopSourceContent = _statementTranslator.Translate(forEachBlock.LoopSrc, scopeAccessInformation, ExpressionReturnTypeOptions.NotSpecified);
-            var undeclaredVariablesInLoopSourceContent = loopSourceContent.GetUndeclaredVariablesAccessed(scopeAccessInformation, _nameRewriter);
-            foreach (var undeclaredVariable in undeclaredVariablesInLoopSourceContent)
-                _logger.Warning("Undeclared variable: \"" + undeclaredVariable.Content + "\" (line " + (undeclaredVariable.LineIndex + 1) + ")");
-            var rewrittenLoopVarName = _nameRewriter.GetMemberAccessTokenName(forEachBlock.LoopVar);
-            var loopVarTargetContainer = scopeAccessInformation.GetNameOfTargetContainerIfAnyRequired(rewrittenLoopVarName, _envRefName, _outerRefName, _nameRewriter);
-            if (loopVarTargetContainer != null)
-                rewrittenLoopVarName = loopVarTargetContainer.Name + "." + rewrittenLoopVarName;
             translationResult = translationResult
                 .Add(new TranslatedStatement(
                     string.Format(
-                        "for each ({0} in {1}.ENUMERABLE({2})",
+                        "foreach ({0} in {1})",
                         rewrittenLoopVarName,
-                        _supportRefName.Name,
-                        loopSourceContent.TranslatedContent
+                        enumerationContent
                     ),
                     indentationDepth
                 ))
@@ -93,11 +134,7 @@ namespace CSharpWriter.CodeTranslation.BlockTranslators
                 Translate(forEachBlock.Statements.ToNonNullImmutableList(), scopeAccessInformation, earlyExitNameIfAny, indentationDepth + 1)
             );
             translationResult = translationResult.Add(new TranslatedStatement("}", indentationDepth));
-            if (scopeAccessInformation.ErrorRegistrationTokenIfAny != null)
-            {
-                indentationDepth--;
-                translationResult = translationResult.Add(new TranslatedStatement("});", indentationDepth));
-            }
+
             var earlyExitFlagNamesToCheck = scopeAccessInformation.StructureExitPoints
                 .Where(e => e.ExitEarlyBooleanNameIfAny != null)
                 .Select(e => e.ExitEarlyBooleanNameIfAny.Name);
--- UnitTests/CSharpWriter/CodeTranslation/IntegrationTests/EndToEndForEachTranslationTests.cs
+++ UnitTests/CSharpWriter/CodeTranslation/IntegrationTests/EndToEndForEachTranslationTests.cs
@@ -15,7 +15,7 @@ namespace VBScriptTranslator.UnitTests.CSharpWriter.CodeTranslation.IntegrationT
             ";
             var expected = new[]
             {
-                "for each (_env.value in _.ENUMERABLE(_env.values)",
+                "foreach (_env.value in _.ENUMERABLE(_env.values))",
                 "{",
                 "    _.CALL(_env.wscript, \"echo\", _.ARGS.Ref(_env.value, v1 => { _env.value = v1; }));",
                 "}"
@@ -27,8 +27,16 @@ namespace VBScriptTranslator.UnitTests.CSharpWriter.CodeTranslation.IntegrationT
         }
 
         /// <summary>
-        /// Error-trapping with For Each loops is must less complicated than For loops (where error-trapping is required around the constraints evaluation - if
-        /// they're not compile-time constants - and then distinct error-trapping around the loop, if the constraint evaluation didn't fail)
+        /// Error-trapping around For Each loops has the curious behaviour that if it is not possible to enumerate over the target (eg. attempting to enumerate
+        /// over a string, which is not supported by VBScript) or if there is an error in evaluating the target (eg. attempting to enumerate over the return
+        /// value of a function where that function raises an error) then the loop will be entered once, but the loop variable's value will not be altered
+        /// (so if it had no value before the loop was reached, it will continue to have no value - but if it already had a value before the loop, this
+        /// value will remain unaltered). If error-trapping may be enabled, then the translated code tries to define a set to enumerate over, leaving
+        /// it null if this fails (and if error-trapping is actually enabled; if it isn't then the error will be raised and the scope exited). When
+        /// the loop is reached, if the target set is null then it falls back to a single-element array whose item is the loop variable's original
+        /// value, which emulates VBScript's behaviour. If there is no ON ERROR RESUME NEXT around the code then none of this extra logic appears
+        /// in the output (see the SimpleCaseWithoutErrorHandling test), but if error-trapping may be enabled then this code must be present
+        /// (even if there's a chance that an ON ERROR GOTO 0 will disable the error-trapping in the current scope at some point at runtime).
         /// </summary>
         [Fact]
         public void SimpleCaseWithErrorHandling()
@@ -43,15 +51,58 @@ namespace VBScriptTranslator.UnitTests.CSharpWriter.CodeTranslation.IntegrationT
             {
                 "var errOn1 = _.GETERRORTRAPPINGTOKEN();",
                 "_.STARTERRORTRAPPINGANDCLEARANYERROR(errOn1);",
+                "IEnumerable enumerationContent2 = null;",
                 "_.HANDLEERROR(errOn1, () => {",
-                "    for each (_env.value in _.ENUMERABLE(_env.values)",
-                "    {",
-                "        _.HANDLEERROR(errOn1, () => {",
-                "            _.CALL(_env.wscript, \"echo\", _.ARGS.Ref(_env.value, v2 => { _env.value = v2; }));",
-                "        });",
-                "    }",
-                "});",
-                "_.RELEASEERRORTRAPPINGTOKEN(errOn1);"
+                "    enumerationContent2 = _.ENUMERABLE(_env.values)",
+                "}",
+                "foreach (_env.value in enumerationContent2 ?? new object[] { _env.value })",
+                "{",
+                "    _.HANDLEERROR(errOn1, () => {",
+                "        _.CALL(_env.wscript, \"echo\", _.ARGS.Ref(_env.value, v3 => { _env.value = v3; }));",
+                "    });",
+                "}",
+                "_.RELEASEERRORTRAPPINGTOKEN(errOn1);",
+
+            };
+            Assert.Equal(
+                expected.Select(s => s.Trim()).ToArray(),
+                WithoutScaffoldingTranslator.GetTranslatedStatements(source, WithoutScaffoldingTranslator.DefaultConsoleExternalDependencies)
+            );
+        }
+
+        /// <summary>
+        /// This is extremely similar to SimpleCaseWithErrorHandling but it demonstrates some of the capabilities and limitations of the static analysis
+        /// regarding potential error-trapping scenarios. The presence of the ON ERROR RESUME NEXT means that the translation process has to consider that
+        /// some error-trapping may occur within the current scope. However, it is not intelligent enough to realise that it is then immediately disabled
+        /// and so the additional error-trapping-related code around the FOR EACH loop is not necessary. It IS intelligent enough to realise that there
+        /// is no scenario where error-trapping could be enabled within the loop, so the WScript.Echo call is not wrapped in a HANDLEERROR call. This
+        /// could be improved in the future but I thought it was worth having this test to illustrate the discrepancy.
+        /// </summary>
+        [Fact]
+        public void SimpleCaseWithErrorHandlingButWithItDisabledAtRunTimeBeforeTheLoop()
+        {
+            var source = @"
+                On Error Resume Next
+                On Error Goto 0
+                For Each value In values
+                    WScript.Echo value
+                Next
+            ";
+            var expected = new[]
+            {
+                "var errOn1 = _.GETERRORTRAPPINGTOKEN();",
+                "_.STARTERRORTRAPPINGANDCLEARANYERROR(errOn1);",
+                "_.STOPERRORTRAPPINGANDCLEARANYERROR(errOn1);",
+                "IEnumerable enumerationContent2 = null;",
+                "_.HANDLEERROR(errOn1, () => {",
+                "    enumerationContent2 = _.ENUMERABLE(_env.values)",
+                "}",
+                "foreach (_env.value in enumerationContent2 ?? new object[] { _env.value })",
+                "{",
+                "    _.CALL(_env.wscript, \"echo\", _.ARGS.Ref(_env.value, v3 => { _env.value = v3; }));",
+                "}",
+                "_.RELEASEERRORTRAPPINGTOKEN(errOn1);",
+
             };
             Assert.Equal(
                 expected.Select(s => s.Trim()).ToArray(),
