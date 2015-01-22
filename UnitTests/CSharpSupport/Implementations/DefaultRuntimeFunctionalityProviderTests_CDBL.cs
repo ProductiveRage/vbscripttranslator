743c64ee2caeb4ee68dcf719b6e069bf51cb9221
dan <742348+ProductiveRage@users.noreply.github.com>
2015-01-22 21:07:37 +0000

    Added tests for CDBL (fairly comprehensive) and LT (some tests to get going)

---
+++ UnitTests/CSharpSupport/Implementations/DefaultRuntimeFunctionalityProviderTests_CDBL.cs
@@ -0,0 +1,181 @@
+﻿using CSharpSupport;
+using CSharpSupport.Attributes;
+using CSharpSupport.Exceptions;
+using System;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace VBScriptTranslator.UnitTests.CSharpSupport.Implementations
+{
+    public static partial class DefaultRuntimeFunctionalityProviderTests
+    {
+        public class CDBL
+        {
+            [Fact]
+            public void Empty()
+            {
+                Assert.Equal(
+                    0d,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(null)
+                );
+            }
+
+            [Fact]
+            public void Null()
+            {
+                Assert.Throws<InvalidUseOfNullException>(() =>
+                {
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(DBNull.Value);
+                });
+            }
+
+            [Fact]
+            public void BlankString()
+            {
+                Assert.Throws<TypeMismatchException>(() =>
+                {
+                    GetDefaultRuntimeFunctionalityProvider().CDBL("");
+                });
+            }
+
+            [Fact]
+            public void NonNumericString()
+            {
+                Assert.Throws<TypeMismatchException>(() =>
+                {
+                    GetDefaultRuntimeFunctionalityProvider().CDBL("a");
+                });
+            }
+
+            [Fact]
+            public void PositiveNumberAsString()
+            {
+                Assert.Equal(
+                    123.4,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL("123.4")
+                );
+            }
+
+            [Fact]
+            public void PositiveNumberAsStringWithLeadingAndTrailingWhitespace()
+            {
+                Assert.Equal(
+                    123.4,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(" 123.4 ")
+                );
+            }
+
+            [Fact]
+            public void NegativeNumberAsString()
+            {
+                Assert.Equal(
+                    -123.4,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL("-123.4")
+                );
+            }
+
+            [Fact]
+            public void Nothing()
+            {
+                var nothing = VBScriptConstants.Nothing;
+                Assert.Throws<ObjectVariableNotSetException>(() =>
+                {
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(nothing);
+                });
+            }
+
+            [Fact]
+            public void ObjectWithoutDefaultProperty()
+            {
+                Assert.Throws<ObjectVariableNotSetException>(() =>
+                {
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(new object());
+                });
+            }
+
+            [Fact]
+            public void ObjectWithDefaultProperty()
+            {
+                var target = new exampledefaultpropertytype { result = 123.4 };
+                Assert.Equal(
+                    123.4,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(target)
+                );
+            }
+
+            [Fact]
+            public void Zero()
+            {
+                Assert.Equal(
+                    0d,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(0)
+                );
+            }
+
+            [Fact]
+            public void PlusOne()
+            {
+                Assert.Equal(
+                    1d,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(1)
+                );
+            }
+
+            [Fact]
+            public void MinusOne()
+            {
+                Assert.Equal(
+                    -1d,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(-1)
+                );
+            }
+
+            [Fact]
+            public void OnePointOne()
+            {
+                Assert.Equal(
+                    1.1d,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(1.1)
+                );
+            }
+
+            [Fact]
+            public void DateAndTime()
+            {
+                Assert.Equal(
+                    42026.8410300926d,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(new DateTime(2015, 1, 22, 20, 11, 5, 0)),
+                    10 // This test fails without specifying precision
+                );
+            }
+
+            [Fact]
+            public void True()
+            {
+                Assert.Equal(
+                    -1d,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(true)
+                );
+            }
+
+            [Fact]
+            public void False()
+            {
+                Assert.Equal(
+                    0d,
+                    GetDefaultRuntimeFunctionalityProvider().CDBL(false)
+                );
+            }
+
+            /// <summary>
+            /// This is an example of the type of class that may be emitted by the translation process, one with a parameter-less default member
+            /// </summary>
+            [TranslatedProperty("ExampleDefaultPropertyType")]
+            private class exampledefaultpropertytype
+            {
+                [IsDefault]
+                public object result { get; set; }
+            }
+        }
+    }
+}
---
+++ UnitTests/CSharpSupport/Implementations/DefaultRuntimeFunctionalityProviderTests_LT.cs
@@ -0,0 +1,107 @@
+﻿using CSharpSupport;
+using CSharpSupport.Exceptions;
+using System;
+using Xunit;
+
+namespace VBScriptTranslator.UnitTests.CSharpSupport.Implementations
+{
+    public static partial class DefaultRuntimeFunctionalityProviderTests
+    {
+        // Note: As with the EQ tests, there won't be a lot of cases here around comparing values extracted from object references since that logic is dealt with
+        // by the VAL method (and once a non-object-reference value has been obtained, the same logic as illustrated below will be followed)
+        public class LT
+        {
+            [Fact]
+            public void EmptyIsNotLessThanEmpty()
+            {
+                Assert.Equal(
+                    false,
+                    GetDefaultRuntimeFunctionalityProvider().LT(null, null)
+                );
+            }
+
+            [Fact]
+            public void NullComparedToNullIsNull()
+            {
+                Assert.Equal(
+                    DBNull.Value,
+                    GetDefaultRuntimeFunctionalityProvider().LT(DBNull.Value, DBNull.Value)
+                );
+            }
+
+            /// <summary>
+            /// Anything compared to Nothing will error, this is just an example case to illustrate that (if ANYTHING would get a free pass it would be DBNull.Value
+            /// but not even it does)
+            /// </summary>
+            [Fact]
+            public void NullComparedToNothingErrors()
+            {
+                var nothing = VBScriptConstants.Nothing;
+                Assert.Throws<ObjectVariableNotSetException>(() =>
+                {
+                    GetDefaultRuntimeFunctionalityProvider().LT(DBNull.Value, nothing);
+                });
+            }
+
+            [Fact]
+            public void NothingComparedToNothingErrors()
+            {
+                var nothing = VBScriptConstants.Nothing;
+                Assert.Throws<ObjectVariableNotSetException>(() =>
+                {
+                    GetDefaultRuntimeFunctionalityProvider().LT(nothing, nothing);
+                });
+            }
+
+            // Empty appears to be treated as zero
+            [Fact]
+            public void ZeroIsNotLessThanEmpty()
+            {
+                Assert.Equal(
+                    false,
+                    GetDefaultRuntimeFunctionalityProvider().LT(0, null)
+                );
+            }
+            [Fact]
+            public void EmptyIsNotLessThanZero()
+            {
+                Assert.Equal(
+                    false,
+                    GetDefaultRuntimeFunctionalityProvider().LT(null, 0)
+                );
+            }
+            [Fact]
+            public void MinusOneIsLessThanEmpty()
+            {
+                Assert.Equal(
+                    true,
+                    GetDefaultRuntimeFunctionalityProvider().LT(-1, null)
+                );
+            }
+            [Fact]
+            public void EmptyIsNotLessThanMinusOne()
+            {
+                Assert.Equal(
+                    false,
+                    GetDefaultRuntimeFunctionalityProvider().LT(null, -1)
+                );
+            }
+            [Fact]
+            public void EmptyIsLessThanPlusOne()
+            {
+                Assert.Equal(
+                    true,
+                    GetDefaultRuntimeFunctionalityProvider().LT(null, 1)
+                );
+            }
+            [Fact]
+            public void PlusOneIsNotLessThanEmpty()
+            {
+                Assert.Equal(
+                    false,
+                    GetDefaultRuntimeFunctionalityProvider().LT(1, null)
+                );
+            }
+        }
+    }
+}
