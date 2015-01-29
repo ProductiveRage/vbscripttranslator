49f8b6187eb3f741d090da51f5c881012c573c1b
dan <742348+ProductiveRage@users.noreply.github.com>
2015-01-29 12:47:10 +0000

    Corrected a type in the Tester example's comments

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -11,7 +11,7 @@ namespace Tester
         {
             // When run, this example code will render an entire class that may be copy-pasted directly into a new file and then executed (so
             // long as the project that it is included in has a reference to "CSharpSupport". The new class has the namer "Runner" and has a
-            // constructor that takes a single argument, it requires a "compability functionality provider". An implementation may be found
+            // constructor that takes a single argument, it requires a "compatibility functionality provider". An implementation may be found
             // in the static class DefaultRuntimeSupportClassFactory. So executing the translated code may be done with:
             //
             //   using (var compatLayer = DefaultRuntimeSupportClassFactory.Get())
