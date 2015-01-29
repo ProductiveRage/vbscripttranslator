1ffed1a334cbce6058bee4b509bee980d899c2d1
dan <742348+ProductiveRage@users.noreply.github.com>
2015-01-29 11:31:06 +0000

    Tweaked the basic example in Tester and explained how to execute the translated code

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -20,14 +20,21 @@ namespace Tester
     {
         static void Main()
         {
+            // When run, this example code will render an entire class that may be copy-pasted directly into a new file and then executed (so
+            // long as the project that it is included in has a reference to "CSharpSupport". The new class has the namer "Runner" and has a
+            // constructor that takes a single argument, it requires a "compability functionality provider". An implementation may be found
+            // in the static class DefaultRuntimeSupportClassFactory. So executing the translated code may be done with:
+            //   using (var compatLayer = DefaultRuntimeSupportClassFactory.Get())
+            //   {
+            //       new Runner(compatLayer).Go();
+            //   }
+            // The Go methods takes an optional "EnvironmentReferences" argument. This has properties for all of the undeclared variables in
+            // the source code. In the example below, you would need to one with a "WScript" implementation (that has a method "Echo" which
+            // takes an object argument and returns a value).
             var content = @"
                 ' Test
-                Dim a
-                Test1 ' Inline comment
-                WScript.Echo 1
-
                 Dim i: For i = 1 To 10
-                    WScript.Echo i
+                    WScript.Echo ""Item"" & i
                 Next";
 
             // Set to Executable to get an entire, runnable program. Set to WithoutScaffolding to see only the meat of the translated content.
