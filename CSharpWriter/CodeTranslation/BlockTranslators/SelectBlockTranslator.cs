a9d62a54d2d0a76f81ee18f99dfae229eb3110ca
DanRoberts <742348+ProductiveRage@users.noreply.github.com>
2015-03-19 22:59:05 +0000

    Added much of the error-trapping code required by the SelectBlockTranslator, there is still more to be done around ensuring that any "ref" variable reference that are part of the containing function or property's argument list that are declared as ByRef are not accessed in lambdas any where (that would be a C# compile error) and some tweaking of the conditions themselves

--- CSharpWriter/CodeTranslation/BlockTranslators/SelectBlockTranslator.cs
+++ CSharpWriter/CodeTranslation/BlockTranslators/SelectBlockTranslator.cs
@@ -51,11 +51,25 @@ namespace CSharpWriter.CodeTranslation.BlockTranslators
             
             // TODO: Special handling for zero cases - the target should still be evaluated (document this)
 
-            // TODO: Error-handling
-
             // TODO: ByRef function argument rewriting (in conjunction with error-handling on on its own)
 
             var translationResult = TranslationResult.Empty;
+
+            // If error-trapping may be enabled at runtime then wrap all of the work in a HANDLEERROR call.  This means that if there is an error raised in evaluating the target expression
+            // that no more work will be attempted - none of the possible case values will be considered for matching. This is correct behaviour and consistent with VBScript,
+            if (scopeAccessInformation.ErrorRegistrationTokenIfAny != null)
+            {
+                translationResult = translationResult.Add(new TranslatedStatement(
+                    string.Format(
+                        "{0}.HANDLEERROR({1}, () => {{",
+                        _supportRefName.Name,
+                        scopeAccessInformation.ErrorRegistrationTokenIfAny.Name
+                    ),
+                    indentationDepth
+                ));
+                indentationDepth++;
+            }
+
             foreach (var openingComment in selectBlock.OpeningComments)
                 translationResult = base.TryToTranslateComment(translationResult, openingComment, scopeAccessInformation, indentationDepth);
 
@@ -78,9 +92,21 @@ namespace CSharpWriter.CodeTranslation.BlockTranslators
                 indentationDepth
             ));
 
-            foreach (var indexedCaseBlock in selectBlock.Content.Select((c, i) => new { Index = i, CaseBlock = c }))
+            var numberOfIndentsRequiredForErrorTrappingStructure = 0;
+            var annotatedCaseBlocks = selectBlock.Content.Select((c, i) => {
+                var lastIndex = selectBlock.Content.Count() - 1;
+                return new
+                {
+                    IsFirstCaseBlock = (i == 0),
+                    IsCaseLastBlockWithValuesToCheck =
+                        ((i == lastIndex) && (c is SelectBlock.CaseBlockExpressionSegment)) ||
+                        ((i == (lastIndex - 1)) && (c is SelectBlock.CaseBlockExpressionSegment) && (selectBlock.Content.Last() is SelectBlock.CaseBlockElseSegment)),
+                    CaseBlock = c
+                };
+            });
+            foreach (var annotatedCaseBlock in annotatedCaseBlocks)
             {
-                var explicitOptionsCaseBlock = indexedCaseBlock.CaseBlock as SelectBlock.CaseBlockExpressionSegment;
+                var explicitOptionsCaseBlock = annotatedCaseBlock.CaseBlock as SelectBlock.CaseBlockExpressionSegment;
                 if (explicitOptionsCaseBlock != null)
                 {
                     var targetAsNumericValueTokenIfApplicable = Is<NumericValueToken>(selectBlock.Expression)
@@ -97,23 +123,85 @@ namespace CSharpWriter.CodeTranslation.BlockTranslators
                         .ToArray(); // Call ToArray since we're always going to enumerate the collection twice below, no point doing the work twice
                     foreach (var undeclaredVariable in conditions.SelectMany(c => c.GetUndeclaredVariablesAccessed(scopeAccessInformation, _nameRewriter)))
                         _logger.Warning("Undeclared variable: \"" + undeclaredVariable.Content + "\" (line " + (undeclaredVariable.LineIndex + 1) + ")");
-                    var combinedCondition = (explicitOptionsCaseBlock.Values.Count() == 1)
-                        ? conditions.Single().TranslatedContent
-                        : ("(" + string.Join(") || (", conditions.Select(c => c.TranslatedContent)) + ")");
+                    
+                    // If error-trapping is not enabled then generate an "if" or "else if" conditional that checks each value using an or, so that if one matches then any subsequent values are not evaluated
+                    if (scopeAccessInformation.ErrorRegistrationTokenIfAny == null)
+                    {
+                        var combinedCondition = (explicitOptionsCaseBlock.Values.Count() == 1)
+                            ? conditions.Single().TranslatedContent
+                            : ("(" + string.Join(") || (", conditions.Select(c => c.TranslatedContent)) + ")");
+                        translationResult = translationResult.Add(new TranslatedStatement(
+                            string.Format(
+                                "{0} ({1})",
+                                annotatedCaseBlock.IsFirstCaseBlock ? "if" : "else if",
+                                combinedCondition
+                            ),
+                            indentationDepth
+                        ));
+                    }
+                    else
+                    {
+                        // If error-trapping IS potentially enabled, then these conditions must each be wrapped in a call to the IF support function that takes the error token and deals with evaluating
+                        // the value; if the evaluation causes an error and error-trapping is enabled at runtime then VBScript would "resume" into the next statement, meaning the block within the CASE
+                        // (and so the IF support function knows to return true in this case).
+                        // - Since this involves more code for each value comparison, if the CASE has multiple values then they will be split onto multiple lines in the emitted C# code
+                        // - Also note that if error-trapping is enabled then we need to change the structure slightly, from "if.. elseif .. else if.. else" to using nested "if.. else { if.. else { } } }"
+                        //   since each condition will be evaulated within a lambda and so may need rewriting if the expression references any ByRef arguments of the containing function (where applicable)
+                        //   because "ref" references may not be accessed within lambdas in C#. TODO: I haven't dealt with this rewriting yet, but I've laid out this structure here so that I'm able to do
+                        //   that work soon.
+                        conditions = conditions
+                            .Select(condition => new TranslatedStatementContentDetails(
+                                string.Format(
+                                    "{0}.IF(() => {1}, {2})",
+                                    _supportRefName.Name,
+                                    condition.TranslatedContent,
+                                    scopeAccessInformation.ErrorRegistrationTokenIfAny.Name
+                                ),
+                                condition.VariablesAccessed
+                            ))
+                            .ToArray();
+                        if (conditions.Count() == 1)
+                        {
+                            translationResult = translationResult.Add(new TranslatedStatement(
+                                string.Format("if ({0})", conditions.Single().TranslatedContent),
+                                indentationDepth
+                            ));
+                        }
+                        else
+                        {
+                            translationResult = translationResult.Add(new TranslatedStatement(
+                                string.Format("if (({0})", conditions.First().TranslatedContent),
+                                indentationDepth
+                            ));
+                            foreach (var condition in conditions.Skip(1).Reverse().Skip(1).Reverse()) // Every condition except the first and last
+                            {
+                                translationResult = translationResult.Add(new TranslatedStatement(
+                                    string.Format("|| ({0})", condition.TranslatedContent),
+                                    indentationDepth
+                                ));
+                            }
+                            translationResult = translationResult.Add(new TranslatedStatement(
+                                string.Format("|| ({0}))", conditions.Last().TranslatedContent),
+                                indentationDepth
+                            ));
+                        }
+                    }
 
-                    translationResult = translationResult.Add(new TranslatedStatement(
-                        string.Format(
-                            "{0} ({1})",
-                            (indexedCaseBlock.Index == 0) ? "if" : "else if",
-                            combinedCondition
-                        ),
-                        indentationDepth
-                    ));
                     translationResult = translationResult.Add(new TranslatedStatement("{", indentationDepth));
                     translationResult = translationResult.Add(
-                        Translate(indexedCaseBlock.CaseBlock.Statements.ToNonNullImmutableList(), scopeAccessInformation, indentationDepth + 1)
+                        Translate(annotatedCaseBlock.CaseBlock.Statements.ToNonNullImmutableList(), scopeAccessInformation, indentationDepth + 1)
                     );
                     translationResult = translationResult.Add(new TranslatedStatement("}", indentationDepth));
+
+                    // See the note above about potentially having to rewrite conditions that include "ref" references - that requires that we change the structure from a more natual "if.. else if.. else if.."
+                    // arrangement to having to nest them (eg. "if { .. else { if.. else { } } }")
+                    if ((scopeAccessInformation.ErrorRegistrationTokenIfAny != null) && !annotatedCaseBlock.IsCaseLastBlockWithValuesToCheck)
+                    {
+                        translationResult = translationResult.Add(new TranslatedStatement("else", indentationDepth));
+                        translationResult = translationResult.Add(new TranslatedStatement("{", indentationDepth));
+                        indentationDepth++;
+                        numberOfIndentsRequiredForErrorTrappingStructure++;
+                    }
                 }
                 else
                 {
@@ -125,7 +213,7 @@ namespace CSharpWriter.CodeTranslation.BlockTranslators
                         indentationDepth++;
                     }
                     translationResult = translationResult.Add(
-                        Translate(indexedCaseBlock.CaseBlock.Statements.ToNonNullImmutableList(), scopeAccessInformation, indentationDepth)
+                        Translate(annotatedCaseBlock.CaseBlock.Statements.ToNonNullImmutableList(), scopeAccessInformation, indentationDepth)
                     );
                     if (!defaultCaseIsTheOnlyCase)
                     {
@@ -133,6 +221,18 @@ namespace CSharpWriter.CodeTranslation.BlockTranslators
                         translationResult = translationResult.Add(new TranslatedStatement("}", indentationDepth));
                     }
                 }
+            }
+            for (var i = 0; i < numberOfIndentsRequiredForErrorTrappingStructure; i++)
+            {
+                indentationDepth--;
+                translationResult = translationResult.Add(new TranslatedStatement("}", indentationDepth));
+            }
+
+            // Close the outer HANDLEERROR wrapper if error-trapping may be enabled
+            if (scopeAccessInformation.ErrorRegistrationTokenIfAny != null)
+            {
+                indentationDepth--;
+                translationResult = translationResult.Add(new TranslatedStatement("});", indentationDepth));
             }
 
             return translationResult;
