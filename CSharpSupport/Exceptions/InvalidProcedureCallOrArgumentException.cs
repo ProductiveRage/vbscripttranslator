4991d2c41f3b95298ecfc5c14d2813cfe673f157
DanRoberts <742348+ProductiveRage@users.noreply.github.com>
2015-05-06 21:37:32 +0100

    Added an InvalidProcedureCallOrArgumentException class to identify these particular types of VBScript error

---
+++ CSharpSupport/Exceptions/InvalidProcedureCallOrArgumentException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CSharpSupport.Exceptions
+{
+    /// <summary>
+    /// This when an invalid type of parameter is specified (such as a non-positive startIndex for the INSTR function)
+    /// </summary>
+    [Serializable]
+    public class InvalidProcedureCallOrArgumentException : SpecificVBScriptException
+    {
+        private const string BASIC_ERROR_DESCRIPTION = "Invalid procedure call or argument";
+
+        public InvalidProcedureCallOrArgumentException(Exception innerException = null) : this(null, innerException) { }
+        public InvalidProcedureCallOrArgumentException(string additionalInformationIfAny, Exception innerException = null)
+            : base(BASIC_ERROR_DESCRIPTION, additionalInformationIfAny, innerException) { }
+
+        protected InvalidProcedureCallOrArgumentException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}
