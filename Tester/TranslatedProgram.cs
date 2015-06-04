206b150fb28408dd41f0347a7761b89b862c13ea
DanRoberts <742348+ProductiveRage@users.noreply.github.com>
2015-06-04 20:50:57 +0100

    Hadn't realised that I'd included the TranslatedProgram.cs file in the Tester project, meaning checkout out the solution fresh gave a build error - whoops! Rather than remove it, I've created it to illustrate what comes out of the example code in Tester's Program.cs

---
+++ Tester/TranslatedProgram.cs
@@ -0,0 +1,66 @@
+﻿/* Undeclared variable: "i" (line 3) */
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using CSharpSupport;
+using CSharpSupport.Attributes;
+using CSharpSupport.Exceptions;
+
+namespace TranslatedProgram
+{
+    public class Runner
+    {
+        private readonly IProvideVBScriptCompatFunctionalityToIndividualRequests _;
+        public Runner(IProvideVBScriptCompatFunctionalityToIndividualRequests compatLayer)
+        {
+            if (compatLayer == null)
+                throw new ArgumentNullException("compatLayer");
+            _ = compatLayer;
+        }
+
+        public void Go()
+        {
+            Go(new EnvironmentReferences());
+        }
+        public void Go(EnvironmentReferences env)
+        {
+            if (env == null)
+                throw new ArgumentNullException("env");
+
+            var _env = env;
+            var _outer = new GlobalReferences(_, _env);
+
+            // Test
+            for (_outer.i = (Int16)1; _.StrictLTE(_outer.i, 10); _outer.i = _.ADD(_outer.i, (Int16)1))
+            {
+                _.CALL(_env.wscript, "Echo", _.ARGS.Val(_.CONCAT("Item", _outer.i)));
+            }
+        }
+
+        public class GlobalReferences
+        {
+            private readonly IProvideVBScriptCompatFunctionalityToIndividualRequests _;
+            private readonly GlobalReferences _outer;
+            private readonly EnvironmentReferences _env;
+            public GlobalReferences(IProvideVBScriptCompatFunctionalityToIndividualRequests compatLayer, EnvironmentReferences env)
+            {
+                if (compatLayer == null)
+                    throw new ArgumentNullException("compatLayer");
+                if (env == null)
+                    throw new ArgumentNullException("env");
+                _ = compatLayer;
+                _env = env;
+                _outer = this;
+                i = null;
+            }
+
+            public object i { get; set; }
+        }
+
+        public class EnvironmentReferences
+        {
+            public object wscript { get; set; }
+            public object i { get; set; }
+        }
+    }
+}
