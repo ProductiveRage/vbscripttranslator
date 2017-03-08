94f0520fcce2536fc81f70a2fe5f7ba48bde0451
Dan Roberts <742348+ProductiveRage@users.noreply.github.com>
2017-03-08 16:26:41 +0000

    Bumped assembly info version to be consistent with NuGet versions

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © ProductiveRage 2016")]
-[assembly: AssemblyVersion("1.0.17.0")]
-[assembly: AssemblyFileVersion("1.0.17.0")]
+[assembly: AssemblyCopyright("Copyright © ProductiveRage 2017")]
+[assembly: AssemblyVersion("1.0.18.0")]
+[assembly: AssemblyFileVersion("1.0.18.0")]
