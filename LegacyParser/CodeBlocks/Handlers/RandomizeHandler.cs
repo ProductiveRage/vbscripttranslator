74d15cd5601abe38fcbfc891b92ca954fac257a7
Dan Roberts <742348+ProductiveRage@users.noreply.github.com>
2017-03-14 15:01:16 +0000

    Don't choke on RANDOMIZE translation in cases where it's the last line and no seed is specified

--- LegacyParser/CodeBlocks/Handlers/RandomizeHandler.cs
+++ LegacyParser/CodeBlocks/Handlers/RandomizeHandler.cs
@@ -5,42 +5,42 @@ using VBScriptTranslator.LegacyParser.Tokens;
 
 namespace VBScriptTranslator.LegacyParser.CodeBlocks.Handlers
 {
-    public class RandomizeHandler : AbstractBlockHandler
-    {
-        /// <summary>
-        /// The token list will be edited in-place as handlers are able to deal with the content, so the input list should expect to be mutated
-        /// </summary>
-        public override ICodeBlock Process(List<IToken> tokens)
-        {
-            if (tokens == null)
-                throw new ArgumentNullException("tokens");
-            if (tokens.Count == 0)
-                return null;
+	public class RandomizeHandler : AbstractBlockHandler
+	{
+		/// <summary>
+		/// The token list will be edited in-place as handlers are able to deal with the content, so the input list should expect to be mutated
+		/// </summary>
+		public override ICodeBlock Process(List<IToken> tokens)
+		{
+			if (tokens == null)
+				throw new ArgumentNullException("tokens");
+			if (tokens.Count == 0)
+				return null;
 
-            // Determine whether we've got a "RANDOMIZE" statement
-            if (!base.checkAtomTokenPattern(tokens, new string[] { "RANDOMIZE" }, false))
-                return null;
+			// Determine whether we've got a "RANDOMIZE" statement
+			if (!base.checkAtomTokenPattern(tokens, new string[] { "RANDOMIZE" }, false))
+				return null;
 
-            // Try to grab the tokens used to declare the seed (this is optional - if
-            // not specified, VBScript uses a time-based value). This section will
-            // throw an exception if invalid tokens are encountered.
-            int tokensProcessed = 1;
-            List<IToken> seedTokens = new List<IToken>();
-            for (int index = 1; index < tokens.Count; index++)
-            {
-                if (base.isEndOfStatement(tokens, index))
-                {
-                    tokensProcessed++;
-                    break;
-                }
-                seedTokens.Add(base.getToken_AtomOrDateStringLiteralOnly(tokens, index));
-                tokensProcessed++;
-            }
+			// Try to grab the tokens used to declare the seed (this is optional - if not specified, VBScript uses a time-based value). This
+			// section will throw an exception if invalid tokens are encountered.
+			var lineIndex = tokens[0].LineIndex; // We'll need this value, so get it before throwing any tokens away
+			int tokensProcessed = 1;
+			List<IToken> seedTokens = new List<IToken>();
+			for (int index = 1; index < tokens.Count; index++)
+			{
+				if (base.isEndOfStatement(tokens, index))
+				{
+					tokensProcessed++;
+					break;
+				}
+				seedTokens.Add(base.getToken_AtomOrDateStringLiteralOnly(tokens, index));
+				tokensProcessed++;
+			}
 
-            // Pull processed tokens from stream and return statement
-            tokens.RemoveRange(0, tokensProcessed);
+			// Pull processed tokens from stream and return statement
+			tokens.RemoveRange(0, tokensProcessed);
 			Expression seedIfAny = (seedTokens.Count == 0 ? null : new Expression(seedTokens));
-			return new RandomizeStatement(lineIndex: tokens[0].LineIndex, seedIfAny: seedIfAny);
-        }
-    }
+			return new RandomizeStatement(lineIndex, seedIfAny: seedIfAny);
+		}
+	}
 }
