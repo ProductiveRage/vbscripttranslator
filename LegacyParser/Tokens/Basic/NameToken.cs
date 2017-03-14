35be62aebe4bea1f3286a2cd0250edc881180349
Dan Roberts <742348+ProductiveRage@users.noreply.github.com>
2017-03-14 15:08:54 +0000

    I thought that all VBScript builtin values and functions would be illegal as variable names but apparently not ("Date" is acceptable, for example) - there is a special list of reserved keywords

--- LegacyParser/Tokens/Basic/NameToken.cs
+++ LegacyParser/Tokens/Basic/NameToken.cs
@@ -2,28 +2,24 @@
 
 namespace VBScriptTranslator.LegacyParser.Tokens.Basic
 {
-    /// <summary>
-    /// This class is used by FunctionBlocks, ValueSettingStatements and other places where content is required that must represent a VBScript name
-    /// </summary>
-    [Serializable]
-    public class NameToken : AtomToken
-    {
-        public NameToken(string content, int lineIndex) : this(content, WhiteSpaceBehaviourOptions.Disallow, lineIndex) { }
+	/// <summary>
+	/// This class is used by FunctionBlocks, ValueSettingStatements and other places where content is required that must represent a VBScript name
+	/// </summary>
+	[Serializable]
+	public class NameToken : AtomToken
+	{
+		public NameToken(string content, int lineIndex) : this(content, WhiteSpaceBehaviourOptions.Disallow, lineIndex) { }
 
-        protected NameToken(string content, WhiteSpaceBehaviourOptions whiteSpaceBehaviour, int lineIndex) : base(content, whiteSpaceBehaviour, lineIndex)
-        {
-            // If this constructor is being called from a type derived from NameToken (eg. EscapedNameToken) then assume that all validation has been
-            // performed in its constructor. If this constructor is being called to instantiate a new NameToken (and NOT a class derived from it) then
-            // use the AtomToken's TryToGetAsRecognisedType method to try to ensure that this content is valid as a name and should not be for a token
-            // of another type. This is process is kind of hokey but I'm trying to layer on a little additional type safety to some very old code so
-            // I'm willing to live with this approach to it (the base class - the AtomToken - having knowledge of all of the derived types is not a
-            // great design decision).
-            if (this.GetType() == typeof(NameToken))
-            {
-                var recognisedType = TryToGetAsRecognisedType(content, lineIndex);
-                if ((recognisedType != null) && !(recognisedType is NameToken))
-                    throw new ArgumentException("Invalid content for a NameToken");
-            }
-        }
-    }
+		protected NameToken(string content, WhiteSpaceBehaviourOptions whiteSpaceBehaviour, int lineIndex) : base(content, whiteSpaceBehaviour, lineIndex)
+		{
+			// If this constructor is being called from a type derived from NameToken (eg. EscapedNameToken) then assume that all validation has been
+			// performed in its constructor. If this constructor is being called to instantiate a new NameToken (and NOT a class derived from it) then
+			// use the AtomToken's TryToGetAsRecognisedType method to try to ensure that this content is valid as a name and should not be for a token
+			// of another type. This is process is kind of hokey but I'm trying to layer on a little additional type safety to some very old code so
+			// I'm willing to live with this approach to it (the base class - the AtomToken - having knowledge of all of the derived types is not a
+			// great design decision).
+			if ((this.GetType() == typeof(NameToken)) && IsReservedKeyword)
+				throw new ArgumentException("Invalid content for a NameToken, may not used reserved keyword \"" + content + "\"");
+		}
+	}
 }
