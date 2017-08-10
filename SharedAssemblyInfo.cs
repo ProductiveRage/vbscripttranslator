168f820bdb6e21a050d3201e85daa34e9ccce38f
Dan Roberts <742348+ProductiveRage@users.noreply.github.com>
2017-08-10 09:28:30 +0100

    Bump to 1.9.1 for Runtime reimplementation of REPLACE

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © ProductiveRage 2017")]
-[assembly: AssemblyVersion("1.9.0.0")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.9.1.0")]
+[assembly: AssemblyFileVersion("1.9.1.0")]
